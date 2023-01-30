hardkernel/u-boot

commit c9a034c47f05eb6693c085d570e2dd1c1db2b9e3

rockchip: board: add sn and ethaddr init control
Allow them to be disabled for critial size platform.

Signed-off-by: Joseph Chen <chenjh@rock-chips.com>
Change-Id: I97089563196585a2846463bf61e8450e9fd66ba8

arch/arm/mach-rockchip/board.c
@@ -81,6 +81,7 @@ __weak int rk_board_init(void)
 	return 0;
 }
 
+#ifdef CONFIG_ROCKCHIP_SET_ETHADDR
 /*
  * define serialno max length, the max length is 512 Bytes
  * The remaining bytes are used to ensure that the first 512 bytes
@@ -140,7 +141,9 @@ static int rockchip_set_ethaddr(void)
 
 	return 0;
 }
+#endif
 
+#ifdef CONFIG_ROCKCHIP_SET_SN
 static int rockchip_set_serialno(void)
 {
 	u8 low[CPUID_LEN / 2], high[CPUID_LEN / 2];
@@ -214,6 +217,8 @@ static int rockchip_set_serialno(void)
 
 	return ret;
 }
+#endif
+
 
 #if defined(CONFIG_USB_FUNCTION_FASTBOOT)
 int fb_set_reboot_flag(void)
@@ -362,8 +367,12 @@ static void cmdline_handle(void)
 
 int board_late_init(void)
 {
+#ifdef CONFIG_ROCKCHIP_SET_ETHADDR
 	rockchip_set_ethaddr();
+#endif
+#ifdef CONFIG_ROCKCHIP_SET_SN
 	rockchip_set_serialno();
+#endif
 	setup_download_mode();
 #if (CONFIG_ROCKCHIP_BOOT_MODE_REG > 0)
 	setup_boot_mode();

arch/arm/mach-rockchip/Kconfig
@@ -881,6 +881,14 @@ config ROCKCHIP_SFC_IOMUX
 	help
 	  This enable U-Boot to config SFC iomux.
 
+config ROCKCHIP_SET_SN
+	bool "Rockchip set serial number"
+	default y
+
+config ROCKCHIP_SET_ETHADDR
+	bool "Rockchip set eth address"
+	default y
+
 config BASE_DEFCONFIG
 	string "Base defconfig of config fragment"
 	default ""

heeplr/u-boot-bananapi-m7

commit 1d020de06d8eee12a63f675e6384b0f4d76887fd

arm64: mach-rockchip: fix build error when SET_ETHADDR=n but SET_SN=y
When disabling SET_ETHADDR but SET_SN is still set as y, the build fails with
error: ‘CPUID_LEN’ undeclared (first use in this function)
and other errors

Fix it by defining macros under the right flags.

Fixes: c9a034c47f05 ("rockchip: board: add sn and ethaddr init control")
Signed-off-by: Eugen Hristev <eugen.hristev@collabora.com>

arch/arm/mach-rockchip/board.c
@@ -81,7 +81,7 @@ __weak int rk_board_init(void)
 	return 0;
 }
 
-#ifdef CONFIG_ROCKCHIP_SET_ETHADDR
+#if defined(CONFIG_ROCKCHIP_SET_ETHADDR) || defined(CONFIG_ROCKCHIP_SET_SN)
 /*
  * define serialno max length, the max length is 512 Bytes
  * The remaining bytes are used to ensure that the first 512 bytes
@@ -92,6 +92,10 @@ __weak int rk_board_init(void)
 #define CPUID_OFF	0x07
 
 #define MAX_ETHERNET	0x2
+
+#endif
+
+#ifdef CONFIG_ROCKCHIP_SET_ETHADDR
 
 static int rockchip_set_ethaddr(void)
 {