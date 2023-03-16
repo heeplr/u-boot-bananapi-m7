90592fe832a8c3635afe3cee65cdc9224ecfa1af
Eugen Hristev <eugen.hristev@collabora.com>
2023-03-16 15:04:33 +0200

    regulator: implement basic reference counter
    Some devices share a regulator supply, when the first one will request
    regulator disable, the second device will have it's supply cut off before
    graciously shutting down. Hence there will be timeouts and other failed
    operations.
    Implement a reference counter mechanism similar with what is done in
    Linux, to keep track of enable and disable requests, and only disable the
    regulator when the last of the consumers has requested shutdown.
    
    Signed-off-by: Eugen Hristev <eugen.hristev@collabora.com>

--- drivers/power/regulator/regulator_common.h
+++ drivers/power/regulator/regulator_common.h
@@ -13,6 +13,7 @@ struct regulator_common_plat {
 	struct gpio_desc gpio; /* GPIO for regulator enable control */
 	unsigned int startup_delay_us;
 	unsigned int off_on_delay_us;
+	unsigned int enable_count;
 };
 
 int regulator_common_of_to_plat(struct udevice *dev,
@@ -20,6 +21,18 @@ int regulator_common_of_to_plat(struct udevice *dev,
 				char *enable_gpio_name);
 int regulator_common_get_enable(const struct udevice *dev,
 	struct regulator_common_plat *dev_pdata);
+/*
+ * Enable or Disable a regulator
+ *
+ * This is a reentrant function and subsequent calls that enable will
+ * increase an internal counter, and disable calls will decrease the counter.
+ * The actual resource will be enabled when the counter gets to 1 coming from 0,
+ * and disabled when it reaches 0 coming from 1.
+ *
+ * @dev: regulator device
+ * @dev_pdata: Platform data
+ * @enable: bool indicating whether to enable or disable the regulator
+ */
 int regulator_common_set_enable(const struct udevice *dev,
 	struct regulator_common_plat *dev_pdata, bool enable);
 
